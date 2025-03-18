d6fa0d713e9104523ff984b27742b6f624707401
chrisiogwaan <chris322322@gmail.com>
2025-03-18 16:58:41 +1100

    +fix: disable mock server for proxy

--- vite.config.ts
+++ vite.config.ts
@@ -35,7 +35,7 @@ export default (configEnv: ConfigEnv): UserConfigExport => {
       /** 接口代理 */
       proxy: {
         "/api/v1": {
-          target: "https://mock.mengxuegu.com/mock/63218b5fb4c53348ed2bc212",
+          target: "",
           ws: true,
           /** 是否允许跨域 */
           changeOrigin: true
